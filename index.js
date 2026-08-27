c4b8158f71baa7b291004004c0296d95db07e8fb
Jordan Smith <tobi657@outlook.com>
2026-08-27 15:47:51 -0400

    adding all files

---
+++ index.js
@@ -0,0 +1,55 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 /20;
+
+function moveBackground (event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX / 20;
+  const y = event.clientY / 20;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+function toggleContrast () {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector('.modal__overlay--loading')
+  const success = document.querySelector('.modal__overlay--success')
+  loading.classList += " modal__overlay--visible";
+  emailjs.sendForm(
+    "service_8kxtjrg",
+    "template_54elmjd",
+    event.target,
+    "CbN1I85o8op-PhFHP",
+  ).then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+  }).catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+      "The email service is temporarily unavailable, please contact me directly at tobi657@outlook.com"
+    );
+  })
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
